13fdeacb8fa91e9490bb33ec8b2ebff22e4fe692
LeonMarqs <leomarques301@gmail.com>
2022-04-10 14:31:01 -0300

    Configuração do Vite e do Prettier

--- src/components/pages/pagina-inicial/index.tsx
+++ src/components/pages/pagina-inicial/index.tsx
@@ -1,30 +1,41 @@
-import { Button, Row } from "antd";
+import { Button, Col, Row } from "antd";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 type PaginaInicialProps = {
-  children?: React.ReactNode; // teste apenas
+	children?: React.ReactNode; // teste apenas
 };
 
-const PaginaInicial = (props: PaginaInicialProps): JSX.Element  => {
+const PaginaInicial = (props: PaginaInicialProps): JSX.Element => {
+	const [count, setCount] = useState<number>(0);
 
-  const [count, setCount] = useState<number>(0);
+	const increment = () => {
+		setCount(count + 1);
+	};
 
-  const increment = () => {
-    setCount(count + 1);
-  }
+	// https://ant.design/components/grid/
 
-  return (
-    <>
-      <Row>
-        <Button type="primary" onClick={increment}>
-          Incrementar
-        </Button>
-      </Row>
-      <Row>
-        {`contador: ${count}`}
-      </Row>
-    </>
-  )
-}
+	return (
+		<>
+			<Row justify="center">
+				<Col>
+					<Button type="primary" onClick={increment}>
+						Incrementar
+					</Button>
+				</Col>
+			</Row>
+
+			<Row justify="space-around">
+				<Col span={6}>{`contador: ${count}`}</Col>
+
+				<Col span={6}>
+					<Button>
+						<Link to={"/teste"}>Ir para teste</Link>
+					</Button>
+				</Col>
+			</Row>
+		</>
+	);
+};
 
 export default PaginaInicial;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,17 @@
-import { render } from 'react-dom';
-import {
-  BrowserRouter,
-  Route,
-  Routes
-} from 'react-router-dom';
+import { render } from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import 'antd/dist/antd.less';
+import "antd/dist/antd.less";
 
-import PaginaInicial from './components/pages/pagina-inicial/index';
+import PaginaInicial from "./components/pages/pagina-inicial";
+import TelaTeste from "./components/pages/teste";
 
 render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<PaginaInicial />} />
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById('root')
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<PaginaInicial />} />
+			<Route path="/teste" element={<TelaTeste />} />
+		</Routes>
+	</BrowserRouter>,
+	document.getElementById("root")
 );
-
