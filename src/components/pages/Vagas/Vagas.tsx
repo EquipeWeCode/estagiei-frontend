39dc9532441bb908435ac6cd59c3c57f4b89a447
luluopa <luluopa@hotmail.com>
2022-08-23 18:05:30 -0300

    feat: criando pagina de vagas gerais

---
+++ src/components/pages/Vagas/Vagas.tsx
@@ -0,0 +1,20 @@
+/// <reference types="vite-plugin-svgr/client" />
+
+import { Row, Col } from "antd"
+
+const Vagas = () => {
+
+    return (
+        <>
+            <Row>
+                <Col>
+                    <h1>Vagas para programadores</h1>
+                    <p>Domina alguma linguagem de programação (Front-End, Back-End, Mobile ou Full Stack)?
+Quer ser contratado como freelancer (PJ), estagiário ou CLT?
+Então você está no lugar certo!</p>
+                </Col>
+            </Row>
+
+        </>
+    )
+}
