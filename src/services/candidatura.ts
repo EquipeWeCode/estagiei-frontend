5a768cf16c69577a5643176e0fafc9bc0aed45a8
Leonardo Marques <leomarques301@gmail.com>
2022-10-13 10:56:32 -0300

    fix: correção merge

--- src/services/candidatura.ts
+++ src/services/candidatura.ts
@@ -1,6 +1,6 @@
 import { ENDPOINT_CANDIDATURA } from "@/constants";
 import { postCandidaturaResource } from "./utils";
 
-export const postCandidatura = (codEstudante: string = "", codVaga: string = "") => {
+export const postCandidatura = (codEstudante: string = "", codVaga: string | number = "") => {
 	return postCandidaturaResource(`${ENDPOINT_CANDIDATURA}/${codEstudante}/${codVaga}`);
 };
