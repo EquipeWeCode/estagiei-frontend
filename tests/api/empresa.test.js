12ca73c39109fdeaa57ae010a3ecb681ec93d706
luluopa <luluopa@hotmail.com>
2022-10-14 20:21:01 -0300

    test: adicionando o teste de api

---
+++ tests/api/empresa.test.js
@@ -0,0 +1,33 @@
+const request = require('supertest');
+const assert = require('assert');
+const ROOT_URL = "https://estagiei.herokuapp.com/api";
+
+const data = {
+    email: "teste@gmail.com",
+    senha: "12345678",
+    razaoSocial: "teste",
+    nomeFantasia: "teste",
+    cnpj: "71366694000",
+    endereco: {
+        cep: "76804149",
+        estado: "RO",
+        cidade: "Porto Velho",
+        bairro: "Nossa Senhora das Graças",
+        logradouro: "Avenida Governador Jorge Teixeira",
+        numero: "21",
+        complemento: "teste",
+        pontoReferencia: "teste"
+    }
+}
+
+describe('POST /empresa [testando a funcionalidade de criar empresas]', function() {
+    it('Deve retornar 202 OK', function(done) {
+      request(ROOT_URL)
+        .post('/empresa/')
+        .send(data)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(202, done);
+    });
+  });
+
