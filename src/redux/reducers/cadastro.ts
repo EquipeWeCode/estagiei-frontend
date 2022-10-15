0ae9f8977ec620b1a54517d9d88205f131ba51e0
luluopa <luluopa@hotmail.com>
2022-10-15 13:34:48 -0300

    feat: adicionando as interfaces nos reducers

--- src/redux/reducers/cadastro.ts
+++ src/redux/reducers/cadastro.ts
@@ -1,11 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface CounterState {
+    cadastroetp1: boolean,
+    cadastroetp2: boolean
+}
+
+const initialState: CounterState = {
+    cadastroetp1: false,
+    cadastroetp2: false
+}
+
 export const counterSlice = createSlice({
     name: 'cadastro',
-    initialState: {
-        cadastroetp1: false,
-        cadastroetp2: false
-    },
+    initialState,
     reducers: {
         negateCadastroetp1: (state) => {
             state.cadastroetp1 = !state.cadastroetp1
