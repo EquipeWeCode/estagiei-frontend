c9a3a179e27db017b3d88be6c8cf0ee36bd9c2a1
Leonardo Marques <leomarques301@gmail.com>
2022-11-16 01:04:45 -0300

    fix: Corrige logout

--- src/services/autenticacao.ts
+++ src/services/autenticacao.ts
@@ -2,6 +2,7 @@ import { ENDPOINT_LOGIN, EXPIRES_IN_KEY } from "@/constants";
 import { LoginType } from "@/types/userTypes";
 import { TOKEN_KEY, USER_KEY } from "./../constants/index";
 import { postResource } from "./utils";
+import instance from "../axios";
 
 export const postLogin = (body: LoginType) => {
 	return postResource(ENDPOINT_LOGIN, body);
@@ -10,6 +11,7 @@ export const postLogin = (body: LoginType) => {
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 
 export const logout = () => {
+	delete instance.defaults.headers.common["Authorization"];
 	localStorage.removeItem(TOKEN_KEY);
 	localStorage.removeItem(USER_KEY);
 	localStorage.removeItem(EXPIRES_IN_KEY);
